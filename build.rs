8bf2ab78208d7792fd29023762af49b5c5bf300e
Cameron Taggart <cameron.taggart@gmail.com>
2019-11-02 01:30:10 +0000

    try on Linux with MaybeUninit error: out of memory or dimension too large for Octave's index type

--- build.rs
+++ build.rs
@@ -1,7 +1,5 @@
 fn main() {
-    println!(r"cargo:rustc-link-search=C:\Octave\Octave-5.1.0.0\mingw64\bin");
-    println!(r"cargo:rustc-link-search=C:\Octave\Octave-5.1.0.0\mingw64\lib");
-    // println!(r"C:\msys64\mingw64\x86_64-w64-mingw32\lib");
-    println!("cargo:rustc-link-lib=octave-7");
-    println!("cargo:rustc-link-lib=octinterp-7");
+    println!("/home/linuxbrew/.linuxbrew/Cellar/octave/5.1.0_6/lib/octave/5.1.0");
+    println!("cargo:rustc-link-lib=octave");
+    println!("cargo:rustc-link-lib=octinterp");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-extern crate octh;
-
 use std::ffi::CString;
+use std::mem::MaybeUninit;
+use core::ffi::c_void;
 
 // https://thefullsnack.com/en/string-ffi-rust.html
 
@@ -22,9 +22,32 @@ pub unsafe extern "C" fn Gadd (shl: *const octh::root::octave::dynamic_library, 
     return fcn;
 }
 
-#[allow(non_snake_case)]
+#[no_mangle]
 pub unsafe extern "C" fn Fadd (_args: *const octh::root::octave_value_list, nargout: i32) -> octh::root::octave_value_list {
+    // https://doc.rust-lang.org/core/mem/union.MaybeUninit.html
     // let out = octh::root::octave_value_list::create();
-    let out = octh::root::octave_value_list_create(nargout);
-    return out;
+    // : octh::root::octave_value_list
+
+    let mut list = MaybeUninit::<octh::root::octave_value_list>::uninit();
+    let out = octh::root::octave_value_list_create(list.as_mut_ptr(), nargout);
+    return list.assume_init();
+    // error: out of memory or dimension too large for Octave's index type
+
+    // let mut list = MaybeUninit::<octh::root::octave_value_list>::uninit();
+    // let out = octh::root::octave_value_list::new(list.as_mut_ptr());
+// 36 |     let out = octh::root::octave_value_list::new(list.as_mut_ptr());
+//    |                                                  ^^^^^^^^^^^^^^^^^ expected array of 3 elements, found struct `octh::root::octave_value_list`
+//    |
+//    = note: expected type `*const [u64; 3]`
+//               found type `*mut octh::root::octave_value_list`
+    // return list.assume_init();
+
+    // let mut list = MaybeUninit::<octh::root::octave_value_list>::uninit();
+    // let mut v = octh::root::octave_value::new(1);
+    // // https://stackoverflow.com/a/24191977/23059
+    // let p_v: *mut c_void = &mut v as *mut _ as *mut c_void;
+    // let list = octh::root::octave_base_value_list_value(p_v);
+    // error: out of memory or dimension too large for Octave's index type
+
+    return list;
 }
